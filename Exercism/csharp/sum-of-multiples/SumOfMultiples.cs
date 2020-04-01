84d57cd629524eec524a51ffa5254fd1fe9c381a
AnnNavarrete <ann.g.navarrete@gmail.com>
2020-04-01 23:29:36 +1100

    Day 1 sum of multiples

---
+++ Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+  public static int Sum(IEnumerable<int> multiples, int max)
+  {
+    var multiplesNum = new List<int>();
+
+    foreach (var num in multiples.Where(oneMultiple => oneMultiple > 0))
+    {
+      for (var i = 1; i < max; i++)
+      {
+        if (i % num == 0)
+        {
+          multiplesNum.Add(i);
+        }
+      }
+    }
+    return multiplesNum.Distinct().Sum();
+  }
+}
