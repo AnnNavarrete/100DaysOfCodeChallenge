4f5fb41a43764a52e665cfb36fd58f5f37b1dd2e
AnnNavarrete <ann.g.navarrete@gmail.com>
2020-04-03 22:36:41 +1100

    Day 3: Continuing working on maths calcs

---
+++ Exercism/csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+  public static int CalculateSquareOfSum(int max)
+  {
+    var sumOfNum = 0d;
+
+    for (var i = 0; i <= max; i++)
+    {
+      sumOfNum += i;
+    }
+    return (int)Math.Pow(sumOfNum, 2d);
+  }
+
+  public static int CalculateSumOfSquares(int max)
+  {
+    var sumOfNum = 0;
+
+    for (var i = 0; i <= max; i++)
+    {
+      sumOfNum += (int)Math.Pow(i, 2d);
+    }
+    return sumOfNum;
+  }
+
+  public static int CalculateDifferenceOfSquares(int max)
+  {
+    return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+  }
+}
