5de58b960fd57e3cc0120ac1f3915fad88ac6cc8
AnnNavarrete <ann.g.navarrete@gmail.com>
2020-07-01 22:49:50 +1000

    Day 91 of #100DaysOfCode

---
+++ Exercism/csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ParallelLetterFrequency
+{
+  public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+  {
+    var letterFrequency = new Dictionary<char, int>();
+
+    foreach (var str in texts)
+    {
+      var letters = str.ToLower().ToCharArray();
+      foreach (var letter in letters)
+      {
+        if (!Char.IsLetter(letter))
+        {
+          continue;
+        }
+        if (letterFrequency.ContainsKey(letter))
+        {
+          letterFrequency[letter]++;
+        }
+        else
+        {
+          letterFrequency.Add(letter, 1);
+        }
+      }
+    }
+    return letterFrequency;
+  }
+}
